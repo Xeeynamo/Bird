c96072d740f1fd8a32a1913eccafd31128aa2117
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-02-13 17:27:04 +0000

    Add tests for different flight trips

--- SharpBird.Tests/MongoBirdTest.cs
+++ SharpBird.Tests/MongoBirdTest.cs
@@ -79,10 +79,43 @@ namespace SharpBird.Tests
             Assert.Equal("mockSrc2", birdSearch.ReceivedCalls().Skip(1).First().GetArguments().First());
         }
 
-        private IEnumerable<FlightModel> GetRandomFlights(int count, int seed = 0) =>
-            Enumerable.Range(0, count).Select(x => GetRandomFlight(count * 1000 + seed * 50 + x));
+        [Fact]
+        public void ShouldReturnOnlyOneKindOfSourceDestinationPair()
+        {
+            const string FlightSrc = "TST1";
+            const string FlightDst = "TST2";
+            var expected = GetRandomFlights(10, 1, FlightSrc, FlightDst).ToList();
 
-        private FlightModel GetRandomFlight(int seed)
+            var birdSearch = Substitute.For<IBirdSearch>();
+            birdSearch.Search(Arg.Is(FlightSrc), Arg.Is(FlightDst), Arg.Any<DateTime>())
+                .Returns(expected);
+            birdSearch.Search(Arg.Is(FlightDst), Arg.Is(FlightSrc), Arg.Any<DateTime>())
+                .Returns(GetRandomFlights(10, 2, FlightDst, FlightSrc));
+            mongoBirdSearch = new MongoBirdSearch(birdSearch, TimeSpan.FromHours(1));
+
+            mongoBirdSearch.Search(FlightSrc, FlightDst, DateTime.Today);
+            mongoBirdSearch.Search(FlightDst, FlightSrc, DateTime.Today);
+
+            var actual = mongoBirdSearch.Search(FlightSrc, FlightDst, DateTime.Today)
+                .ToList();
+
+            Assert.All(actual, x =>
+            {
+                Assert.Equal(FlightSrc, x.Origin);
+                Assert.Equal(FlightDst, x.Destination);
+            });
+
+            Assert.Equal(expected.Count, actual.Count);
+            Assert.All(actual, act =>
+            {
+                Assert.True(expected.Any(exp => exp.Id == act.Id));
+            });
+        }
+
+        private IEnumerable<FlightModel> GetRandomFlights(int count, int seed = 0, string origin = null, string destination = null) =>
+            Enumerable.Range(0, count).Select(x => GetRandomFlight(count * 1000 + seed * 50 + x, origin, destination));
+
+        private FlightModel GetRandomFlight(int seed, string origin, string destination)
         {
             var today = DateTime.Today.AddDays(seed);
 
@@ -91,8 +124,8 @@ namespace SharpBird.Tests
                 Id = Guid.NewGuid(),
                 Provider = "testing",
                 ProviderId = PseudoRandomUtil.RandomString(seed, 16),
-                Origin = PseudoRandomUtil.RandomString(seed, 3),
-                Destination = PseudoRandomUtil.RandomString(seed, 4),
+                Origin = origin ?? PseudoRandomUtil.RandomString(seed, 3),
+                Destination = destination ?? PseudoRandomUtil.RandomString(seed, 4),
                 TimeDeparture = today,
                 TimeArrival = today.AddMinutes(95),
                 TimeDepartureUtc = today,
